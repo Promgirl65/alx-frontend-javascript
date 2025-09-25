23e8d7e6e622943afdeb1529a2ee759aa0a01f58
Promgirl65 <olayinkabusolami@gmail.com>
2025-09-25 14:10:27 +0100

    task_3

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,10 @@
+// Define a type RowID as a number
+export type RowID = number;
+
+// Define an interface RowElement
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number; // optional
+}
+
